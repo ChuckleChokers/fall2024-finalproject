705364cdb14ac5a1939eb71b3e5ba1d62b582f9a
Indi Harris <iharris002@drury.edu>
2024-12-03 13:17:42 -0600

    App.js update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+mongoose.connect(
+"mongodb://localhost:27017/newCollection",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+const contactSchema = {
+    email: String,
+    query: String,
+};
+
+const Contact =
+    mongoose.model("Contact", contactSchema);
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/contact",
+    function (req, res) {
+        res.render("contact");
+    });
+
+app.post("/contact",
+    function (req, res) {
+        console.log(req.body.email);
+        const contact = new Contact({
+            email: req.body.email,
+            query: req.body.query,
+        });
+        contact.save(function (err) {
+            if (err) {
+                throw err;
+            } else {
+                res.render("contact");
+            }
+        });
+    });
+
+app.listen(3000,
+    function () {
+        console.log("App is running on Port 3000");
+    });
 
 export default App;
