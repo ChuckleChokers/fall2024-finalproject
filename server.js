a7c5f30a041f372170f47bfee56129f812ffee32
lnsny <125707663+lnsny@users.noreply.github.com>
2024-12-11 10:06:58 -0600

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
 // Connect MongoDB
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error("Error connecting to MongoDB:", error));
 
